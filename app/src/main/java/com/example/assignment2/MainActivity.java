f813108ab6fe136461ca0eb2baca60318fc8ef8f
MoazAlshaer <moaz.shaer2019@gmail.com>
2023-03-18 14:00:06 +0200

    first push

---
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UploudPDf.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
